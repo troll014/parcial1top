fe973f8faa372b5b915aab291199994df4c0a377
troll014 <112781072+troll014@users.noreply.github.com>
2025-06-29 10:42:39 -0500

    Update firebase.ts
    Se modificó la forma de usar las credenciales.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
+/*import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -7,6 +7,31 @@ const firebaseConfig = {
   authDomain: "noticia-ca561.firebaseapp.com",
   projectId: "noticia-ca561",
   storageBucket: "noticia-ca561.firebasestorage.app",
+  messagingSenderId: "1026672530383",
+  appId: "1:1026672530383:web:4c58d245d3f0220fa3f8ba",
+  measurementId: "G-2NETXN0GSX"
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
+//arriba es comentario */
+
+
+
+
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6EOnKFTOwMgayzpXtULph83Y2I8IvY_c",
+  authDomain: "noticia-ca561.firebaseapp.com",
+  projectId: "noticia-ca561",
+  storageBucket: "noticia-ca561.appspot.com", // Corregido aquí
   messagingSenderId: "1026672530383",
   appId: "1:1026672530383:web:4c58d245d3f0220fa3f8ba",
   measurementId: "G-2NETXN0GSX"
